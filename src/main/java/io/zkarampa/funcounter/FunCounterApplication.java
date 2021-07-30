e1d56c6a3f3d18247e92709b06b225fa250f616f
Zisis Karampaglis <7737887+zkarampa@users.noreply.github.com>
2021-07-30 23:28:11 +0100

    Change to redis

---
+++ src/main/java/io/zkarampa/funcounter/FunCounterApplication.java
@@ -0,0 +1,13 @@
+package io.zkarampa.funcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunCounterApplication.class, args);
+	}
+
+}
--- src/test/java/io/zkarampa/funcounter/FunCounterApplicationTests.java
+++ src/test/java/io/zkarampa/funcounter/FunCounterApplicationTests.java
@@ -1,13 +1,13 @@
-package io.zkarampa.funtracker;
+package io.zkarampa.funcounter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FunTrackerApplicationTests {
+class FunCounterApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
