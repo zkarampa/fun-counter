c8eea7c78107c0dc488b9c0d03c66c0995609a5d
Zisis Karampaglis <7737887+zkarampa@users.noreply.github.com>
2021-07-30 23:06:08 +0100

    Initial commit

---
+++ src/test/java/io/zkarampa/funtracker/FunTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zkarampa.funtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
